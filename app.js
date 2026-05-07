767bef54721615bf192c1902a11cd8a94b51ae9e
Ernest V <ernest.viol@gmail.com>
2026-05-07 09:18:45 -0700

    feat: Include middleware
    Moved config files to lib/config to initialize passport and session

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import express from 'express';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
+import passport from './lib/config/passport.js';
+import session from './lib/config/session.js';
 
+// Initialize variables
 const PORT = process.env.PORT || 3000;
 const viewsPath = path.join(
   path.dirname(fileURLToPath(import.meta.url)),
@@ -12,14 +15,30 @@ const publicPath = path.join(
   'public',
 );
 
+// Start express and configure
 const app = express();
-
 app.set('views', viewsPath);
 app.set('view engine', 'ejs');
 
+// Set middleware
 app.use(express.static(publicPath));
 app.use(express.urlencoded({ extended: true })); // traditional html forms application/x-www-form-urlencoded
-// app.use(express.json()); // apy requests application/json
+// app.use(express.json()); // api requests application/json
+
+app.use(session); // express session with pg session configured
+app.use(passport.session());
+app.use(passport.initialize());
+
+app.use((req, res, next) => {
+  if (req.user) {
+    res.locals.currentUser = req.user;
+  }
+  next();
+}); // Automatically set currentUser on views
+
+// Routes
+
+// Error handling
 
 app.listen(PORT, (error) => {
   if (error) throw error;
